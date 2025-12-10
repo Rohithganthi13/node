bdb9936c855d957deba1e6d33a218f70cf802fba
Rohithganthi13 <rohithganthi@gmail.com>
2025-12-10 14:52:38 +0530

    added ejs

--- app.js
+++ app.js
@@ -4,18 +4,9 @@ const bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin.js");
 const shopRoutes = require("./routes/shop.js");
 const rootDir = require("./utils/path.js");
-const { engine } = require("express-handlebars");
 const app = express();
 
-app.engine(
-  "hbs",
-  engine({
-    extname: "hbs",
-    layoutsDir: "views/layouts",
-    defaultLayout: "main-layout",
-  })
-);
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,7 +16,7 @@ app.use(shopRoutes);
 
 app.use((req, res, next) => {
   // res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
-  res.status(404).render("404", { pageTitle: "Page not found" });
+  res.status(404).render("404", { pageTitle: "Page not found", path: "" });
 });
 
 app.listen(3000, () => {
